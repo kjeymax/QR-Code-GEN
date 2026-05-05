28570fcbb63bd2fb85e5b43121e07be7ed5fbb12
Kavindu AJ <70721362+kjeymax@users.noreply.github.com>
2026-05-05 12:35:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// GitHub repository name
+const repoName = 'QR-Code-generator-2';
+
 export default defineConfig({
   plugins: [react()],
+  base: `/${repoName}/`,
 });
